2ab3dfacf8d0c68099525cb64f95f775af764d5e
Kiady Bezaka <kiady.razafindrabe@gmail.com>
2024-07-29 10:47:19 +0200

    [init] add initial files

---
+++ Java/Java.java
@@ -0,0 +1,3 @@
+// Given a list of temperatures, find closest temperature to zero degree Celsius
+// Temperature can be expressed in Celsius or in Farenheit (T(F) = T(C) * 9/5 + 32)
+// Input example : [ '10 C', '-10 F', '5 C', '8 C', '-7 F', '5 C' ] 
---
+++ WP/WP.js
@@ -0,0 +1,8 @@
+var tasks = [{
+    "date": "29/07/2024",
+    "ticket": [{
+        "id":"11463",
+        "time": "2h",
+        "logged": true,
+    }]
+}]
