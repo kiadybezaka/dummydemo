1d6166b9ac7f9a7b6dd1a862540733aec3763a50
Kiady Bezaka <kiady.razafindrabe@gmail.com>
2024-07-30 12:17:14 +0200

    [XL] Add day

---
+++ WP/XL.js
@@ -0,0 +1,61 @@
+var oneDays = [
+    {
+        date: "29/07/2024",
+        activity: [
+            {
+                timeStart: "06:30",
+                timeEnd: "",
+                type: "WAKE_UP",
+                comment: "",
+            },
+            {
+                timeStart: "06:40",
+                timeEnd: "",
+                type: "CHANGE_CACA",
+                comment: "",
+            },
+            {
+                timeStart: "07:00",
+                timeEnd: "07:10",
+                type: "EAT",
+                comment: "120ml",
+            },
+            {
+                timeStart: "08:30",
+                timeEnd: "11:00",
+                type: "NAP",
+                comment: "",
+            },
+            {
+                timeStart: "11:00",
+                timeEnd: "",
+                type: "CHANGE_PIPI",
+                comment: "",
+            },
+            {
+                timeStart: "12:00",
+                timeEnd: "12:20",
+                type: "EAT",
+                comment: "Carotte+PommeDeTerre+Courgette / Compote",
+            },
+            {
+                timeStart: "15:30",
+                timeEnd: "",
+                type: "CHANGE_PIPI",
+                comment: "",
+            },
+            {
+                timeStart: "16:30",
+                timeEnd: "16:45",
+                type: "EAT",
+                comment: "Yahourt / Compote",
+            },
+            {
+                timeStart: "20:00",
+                timeEnd: "20:30",
+                type: "EAT",
+                comment: "Carotte+PommeDeTerre+Courgette / Compote / 60ml",
+            },
+        ]
+    }
+]
